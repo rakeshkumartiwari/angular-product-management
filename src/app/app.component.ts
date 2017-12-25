bb1efc5ef7d5b024a3624017550dda674fb52c87
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-12-25 23:41:31 +0530

    Angular First Componrnt

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+selector: 'pm-root',
+template: `
+<div><h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+</div>
+`
+})
+
+// tslint:disable-next-line:one-line
+export class AppComponent{
+pageTitle: string= 'Project Management';
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'pm-products',
+templateUrl: './product-list.component.html'
+})
+
+// tslint:disable-next-line:one-line
+export class ProductListComponent{
+
+}
