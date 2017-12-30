9da1a52eade3e8aa2206415fdc76731264c799c2
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-12-31 03:41:28 +0530

    created Interface

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -13,7 +14,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'Cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             'productId': 1,
             'productName': 'Leaf Rake',
