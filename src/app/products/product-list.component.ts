45fa3a4cb8d2f096518d9b4b4bda4b39822d47a7
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2018-01-02 01:32:51 +0530

    added filter functionality

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,7 +14,15 @@ export class ProductListComponent {
     imageWidth: number = 30;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'Cart';
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this._listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             'productId': 1,
@@ -67,6 +75,17 @@ export class ProductListComponent {
             'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
         }
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'Cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
